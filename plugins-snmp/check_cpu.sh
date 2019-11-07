324c11ab27bf05c6d50de94898624611fe718535
BRVN01 <39762901+BRVN01@users.noreply.github.com>
2019-11-07 14:24:24 -0300

    Create check_cpu.sh

---
+++ plugins-snmp/check_cpu.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# http://oidref.com/1.3.6.1.4.1.2021.11.11
+
+# Usar a OID .1.3.6.1.4.1.2021.11.11.0
+
+host=${1}
+version=${2}
+community=${3}
+oid=${4}
+
+warning=${5}
+critical=${6}
+
+idle=`snmpget -Otv -${version} -c ${community} ${host} ${oid} 2> /dev/null | awk '{print $2}'`
+
+echo ${idle} | grep -Eq "^[0-9]{1,3}$" || {
+  echo "UNKNOWN: unexpected data"
+  exit 3
+}
+
+cpu=$((100-${idle}))
+
+if [ ${cpu} -ge ${critical} ]; then
+  echo "CRITICAL: CPU usage is ${cpu}%"
+  exit 2
+fi
+
+if [ ${cpu} -ge ${warning} ]; then
+  echo "WARNING: CPU usage is ${cpu}%"
+  exit 1
+fi
+
+echo "OK: CPU usage is ${cpu}%"
+exit 0
