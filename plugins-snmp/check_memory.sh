e43aa0ec767cd468df37d383bbc0e6076d7d48d5
BRVN01 <39762901+BRVN01@users.noreply.github.com>
2019-11-23 13:51:11 -0300

    Update check_memory.sh

--- plugins-snmp/check_memory.sh
+++ plugins-snmp/check_memory.sh
@@ -5,20 +5,19 @@ commnuty=$2
 warning=$3
 critical=$4
 
-HOST=127.0.0.1
-commnuty=public
-
 CMD="snmpget -OQv -v2c -c $commnuty $HOST"
 
 OIDs[0]=".1.3.6.1.4.1.2021.4.5.0"
 OIDs[1]=".1.3.6.1.4.1.2021.4.11.0"
 OIDs[2]=".1.3.6.1.4.1.2021.4.14.0"
 OIDs[3]=".1.3.6.1.4.1.2021.4.15.0"
+OIDs[4]=".1.3.6.1.4.1.2021.4.3.0"
 
 loop="memory_total
 memory_free
 memory_buff
-memory_cache"
+memory_cache
+memory_total_swap"
 
 count="0"
 
@@ -29,16 +28,17 @@ do
     count=$(($count+1))
 done
 
+memory_free=$(($memory_free-$memory_total_swap))
+
 used=$(($memory_total-($memory_free+$memory_buff+$memory_cache+70)))
 
-
 if [ ${used} -ge ${critical} ]; then
-  echo "CRITICAL: CPU usage is ${used} MB"
-  exit 2
+	echo "CRITICAL: CPU usage is ${used} MB"
+	exit 2
 
 elif [ ${used} -ge ${warning} ]; then
-  echo "WARNING: CPU usage is ${used} MB"
-  exit 1
+	echo "WARNING: CPU usage is ${used} MB"
+	exit 1
 fi
 
 echo "OK: Memory usage is ${used} MB"
